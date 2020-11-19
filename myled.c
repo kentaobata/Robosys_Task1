a081e00914559ea041b5a447bb466754d9ccd740
Kenta Obata <usedgit@gmail.com>
2020-11-19 13:43:28 +0000

    fix a file

--- myled.c
+++ myled.c
@@ -14,8 +14,10 @@ static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
 
+static volatile u32 *gpio_base = NULL;
+
 static ssize_t  led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
-	{
+{
 	
 	char c;
 	if(copy_from_user(&c,buf,sizeof(char)))
