74cb8bec3506f658be9ae00fa4d05dcec1c2f1fd
Kenta Obata <usedgit@gmail.com>
2020-11-19 13:36:45 +0000

    fix a file

--- myled.c
+++ myled.c
@@ -25,13 +25,6 @@ static ssize_t  led_write(struct file* filp, const char* buf, size_t count, loff
 	return 1;
 }
 
-	char c;
-	if(copy_from_user(&c,buf,sizeof(char)));
-		return -EFAULT;
-
-	printk(KERN_INFO "receave %c\n", c);
-	return 1;
-
 static ssize_t  sushi_read(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 	int size = 0;
@@ -48,10 +41,6 @@ static struct file_operations led_fops = {
 	.owner = THIS_MODULE,
 	.write = led_write,
 	.read = sushi_read
-};
-static struct file_operations led_fops = {
-	.owner = THIS_MODULE,
-	.write = led_write
 };
 
 static int __init init_mod(void)
